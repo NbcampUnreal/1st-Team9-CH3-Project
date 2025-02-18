20e1f113721bcd87f8f7cc87b63fb084c7a5d48d
cmgeugene <jscj0815@gmail.com>
2025-02-18 15:54:29 +0900

    test : c++ 클래스 생성
    언리얼 에디터에서 c++ 클래스 작성 후 깃헙 데스크탑에서 커밋하여 정상적으로 올라가는지 확인

---
+++ LostInEden/Source/LostInEden/Public/TestEnemyAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TestEnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOSTINEDEN_API ATestEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
